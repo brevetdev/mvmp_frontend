263e5fa2cd8d8f1e16c834c541f33b18e8aefcf8
StevenAza <nazamado569@gmail.com>
2020-12-17 00:03:11 -0500

    :lipstick: Cambios en Frontend

--- src/views/Archivos.vue
+++ src/views/Archivos.vue
@@ -81,69 +81,57 @@
             :gutter="10"
             columnWidth="190000"
           >
+          
             <div
               
               v-masonry-tile
               class="item mansory-item "
-              v-for="(item, index) in listaArchivos.EntradaImgen"
-              :key="index"
+              v-for="(item, index) in objRandom"
+              :key="index" 
             >
-             <div class="mansory-item__imagen">
+             <div class="mansory-item__imagen" v-if="item.tipo==='Imagen'" @click.prevent="showFiles(item)">
               <img v-bind:src="apiUrl + item.imagen.url" alt="" v-bind:height="item.imagen.height" v-bind:width="item.imagen.width" />
               <p class="mansory-item__imagen__cita">{{ item.pieImagen }}</p>
              </div>
-            </div>
-            <div
-              v-masonry-tile
-              class="item mansory-item grid"
-              v-for="(itemA, indexA) in listaArchivos.EntradaAudio"
-              :key="indexA"
-            >
-                <div class="mansory-item__audio">
+            <div class="mansory-item__audio" v-if="item.tipo === 'Audio'" @click.prevent="showFiles(item)">
                <vue-plyr ref="miaudio" class="audio_container__reproductor">
-              
                 <audio controls crossorigin playsinline>
                   <source
-                    v-bind:src="apiUrl + itemA.audio.url"
+                    v-bind:src="apiUrl + item.audio.url"
                     type="audio/mp3"
                   />
                 </audio>
-             
               </vue-plyr>
-                 <div class="mansory-item__audio__cita" v-html="parseoHtml(itemA.piedeAudio)"></div>
+                 <div class="mansory-item__audio__cita" v-html="parseoHtml(item.piedeAudio)"></div>
             </div>
-            </div>
-            <div
-              v-masonry-tile
-              class="item mansory-item"
-              v-for="(itemV, indexV) in listaArchivos.entradaVideo"
-              :key="indexV"
-            >
-            <div class="mansory-item__video">
+            <div class="mansory-item__video"  @click.prevent="showFiles(item)" v-if="item.tipo==='Video'">
                <vue-plyr clickToPlay="true" :enabled="false" ref="mivideo" class="video_container__reproductor">
                 <video  crossorigin playsinline>
                   <source
-                    v-bind:src="apiUrl + itemV.video[0].url"
+                    v-bind:src="apiUrl + item.video[0].url"
                     type="video/mp4"
                   />
                 </video>
               </vue-plyr>
-              <div class="mansory-item__video__cita" v-html="parseoHtml(itemV.pieVideo)"></div>
+              <div class="mansory-item__video__cita" v-html="parseoHtml(item.pieVideo)"></div>
             </div>
+            <div class="mansory-item__blog" @click.prevent="showFiles(item)" v-if="item.tipo==='Blog'">
+                <div class="mansory-item__blog__resumen" v-html="parseoHtml(cutResume(item.ResumenArticulo))"></div>
+               <div class="mansory-item__blog__articulo"  v-html="parseoHtml(cutResume(item.citaArticulo))"></div> 
             </div>
-             <div
-              v-masonry-tile
-              class="item mansory-item grid"
-              v-for="(objBlog, indBlog) in listaArchivos.entradaBlog"
-              :key="indBlog"
-            >
-            <div class="mansory-item__blog">
-                <div class="mansory-item__blog__resumen" v-html="parseoHtml(objBlog.ResumenArticulo)"></div>
-               <div class="mansory-item__blog__articulo"  v-html="parseoHtml(objBlog.citaArticulo)"></div> 
-            </div>
-
             </div>
           </div>
+      </div>
+      <div class="archivos__contenidos__interna" v-if="showingFile">
+        <div class="archivos_interna__options" @click.prevent="closeInt" >
+              <img src="../assets/img/close-icon.svg" alt="">
+        </div>
+        <div class="container__interna">
+              <div class="interna-item__imagen" >
+               <img v-bind:src="apiUrl + InternaFiles.imagen.url" alt="" v-bind:height="InternaFiles.imagen.height" v-bind:width="InternaFiles.imagen.width" />
+                <p class="interna-item__imagen__cita">{{ InternaFiles.pieImagen }}</p> 
+             </div>
+        </div>
       </div>
     </div>
   </div>
@@ -158,6 +146,10 @@ export default {
   name: "Archivos",
   mounted() {
     this.infoExpo();
+     window.onscroll = function() {
+       this.actualScroll = window.scrollY;
+       console.log(this.actualScroll);
+    }
   },
   components: {
     Carousel3d,
@@ -169,6 +161,10 @@ export default {
       apiUrl: process.env.API,
       imagenesDescatadas: [],
       player: undefined,
+      objRandom: [],
+      actualScroll: 0,
+      InternaFiles: [],
+      showingFile: false,
       anchoPantalla: window.screen.width - 400,
       altoPantalla: window.screen.height - 300,
       flagLoadArchivos: false,
@@ -190,26 +186,66 @@ export default {
     dropdown: dropdown,
   },
   methods: {
+    showFiles(itemM) {
+      this.InternaFiles = itemM;
+      let bodyEl = document.querySelector('body');
+      bodyEl.style.overflow ="hidden";
+      window.scrollTo({top: 0})
+      this.showingFile = true;
+    },
+    closeInt() {
+      alert(this.actualScroll);
+      let bodyEl = document.querySelector('body');
+      bodyEl.style.overflow ="auto";
+      window.scrollTo({top: this.actualScroll})
+      this.showingFile = false;
+    },
     infoExpo() {
       let urlExpo = process.env.API + "/archivos";
       axios.get(urlExpo).then((response) => {
-        this.flagLoadArchivos = true;
         this.listaArchivos = response.data;
-        console.log(this.listaArchivos);
-
-        console.log("DESDE ACÁ .__.", this.listaArchivos.EntradaImgen);
-        this.listaArchivos.EntradaImgen.forEach((element) => {
-          if (element.destacado) {
-            this.imagenesDescatadas.push(element);
-          }
-        });
+        this.sortRand();
+        this.flagLoadArchivos = true;
         try {
           this.$refs.miaudio.player;
           this.$refs.mivideo.player;
         } catch (e) {}
       });
     },
-    countContent() {},
+    sortRand() {
+      this.listaArchivos.EntradaImgen.forEach((element) => {
+      element.tipo = "Imagen";
+      if (element.destacado) {
+        this.imagenesDescatadas.push(element);
+      }
+      this.objRandom.push(element);
+      });
+      this.listaArchivos.EntradaAudio.forEach((elementA) => {
+      elementA.tipo = "Audio";
+      this.objRandom.push(elementA);
+      });
+      this.listaArchivos.entradaVideo.forEach((elementV) => {
+      elementV.tipo = "Video";
+      this.objRandom.push(elementV);
+      });
+
+      this.listaArchivos.entradaBlog.forEach((elementB) => {
+      console.log(elementB);
+      elementB.tipo = "Blog";
+      this.objRandom.push(elementB);
+      });
+
+      this.objRandom.sort(function() { return Math.random() - 0.5 });
+      console.log(this.objRandom);
+    },
+    cutResume(textC) {
+        // Length to cut 
+        let lengthToCut = 180; 
+        // Sample text var text = "The quick brown fox jumps over the lazy dog";
+         // We are getting 50 letters (0-50) from sample text 
+         let cutted = textC.substr(0, lengthToCut);
+          return cutted+'....';
+    },
     methodToRunOnSelect(payload) {
       this.object = payload;
     },
